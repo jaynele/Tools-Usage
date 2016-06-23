c60576aea5cb22fa342f8ce5eb822a38fe01ba22
jaynele <1798805740@qq.com>
2016-06-23 14:09:08 +0800

    git

---
+++ git-bash.php
@@ -0,0 +1,40 @@
+<?php 
+////////windows.github.com  we get it...
+//git-for-windows.github.io   
+//git bash
+//$ git config --global user.name
+//cd ..
+//mkdir
+//git init
+/////////vim . sublim .phpstorm edit it
+//git status
+//git add .
+//git commit -m "push something"
+/////////when we update the file,we also should not forget some action as below
+//git add .
+//git commit -m "update something"
+/////////when we want to add some files and at the same time delete some files we can try it like this...
+//touch ..
+//git add .
+//git commit -m ""
+//ls
+//git rm 
+//git commit -m ""
+//ls
+///////here we should create a new repository 
+//git remote add origin https://github.com/jaynele/test.git
+//git pull origin master
+//git push origin master
+//////another person work with you 
+//////we should need a organization and need another person work with us
+//git remote add ori https://github.com/oncejay/test.git
+//git pull ori master
+//git push ori master
+//cd ..
+//git clone https://github.com/oncejay/test.git
+//cd ..
+//ls
+
+
+
+
